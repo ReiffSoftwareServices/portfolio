5363bbf367e7d3014a5b901b6682480e828938c7
Jan Reiff <Jan Reiff>
2018-07-15 15:00:16 +0200

    correction

--- js/main.js
+++ js/main.js
@@ -6,12 +6,9 @@ xhttp.onreadystatechange = function() {
        document.getElementById("demo").innerHTML = xhttp.responseText;
     }
 };
-xhttp.open("GET", "http://www.omdbapi.com/?i=tt3896198&apikey=28a5839e", true);
-xhttp.send();
+//xhttp.open("GET", "http://www.omdbapi.com/?i=tt3896198&apikey=28a5839e", true);
+//xhttp.send();
 
-
-var date_text = Date();
-console.log(date_text);
 var request_text1 = "http://webservices.amazon.com/onca/xml?Service=AWSECommerceService&AWSAccessKeyId=AKIAJMVPMMG6P7HCWLPQ&AssociateTag=codenundgutau-21&Operation=ItemLookup&ItemId=B00008OE6I&Timestamp=[YYYY-MM-DDThh:mm:ssZ]&Signature=[Request Signature]"
 var request_text2 = "http://webservices.amazon.com/onca/xml?Service=AWSECommerceService&AWSAccessKeyId=AKIAJMVPMMG6P7HCWLPQ&AssociateTag=codenundgutau-21&Operation=ItemLookup&ItemId=0679722769&ResponseGroup=Images%2CItemAttributes%2COffers%2CReviews&Version=2013-08-01&Timestamp=2014-08-18T12%3A00%3A00Z"
 var request_text3 = "http://webservices.amazon.com/onca/xml?Service=AWSECommerceService&Operation=ItemLookup&ResponseGroup=Images&IdType=ASIN&&ItemId=B004HO6I4M&AWSAccessKeyId=AKIAJMVPMMG6P7HCWLPQ&AssociateTag=codenundgutau-21&Timestamp=[YYYY-MM-DDThh:mm:ssZ]&Signature=[Request_Signature]"
