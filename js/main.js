b321bdeea8526e453fb94e4a7e7fff777833bb07
Jan Reiff <Jan Reiff>
2018-07-15 15:47:20 +0200

    test

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ xhttp.onreadystatechange = function() {
 //xhttp.send();
 
 var request_text1 = "http://webservices.amazon.com/onca/xml?Service=AWSECommerceService&AWSAccessKeyId=AKIAJMVPMMG6P7HCWLPQ&AssociateTag=codenundgutau-21&Operation=ItemLookup&ItemId=B00008OE6I&Timestamp=[YYYY-MM-DDThh:mm:ssZ]&Signature=[Request Signature]"
-var request_text2 = "https://webservices.amazon.com/onca/xml?Service=AWSECommerceService&AWSAccessKeyId=AKIAJMVPMMG6P7HCWLPQ&AssociateTag=codenundgutau-21&Operation=ItemLookup&ItemId=0679722769&ResponseGroup=Images%2CItemAttributes%2COffers%2CReviews&Version=2013-08-01&Timestamp=2014-08-18T12%3A00%3A00Z"
+var request_text2 = "https://webservices.amazon.de/onca/xml?AWSAccessKeyId=AKIAILJU5AEZIWQRBT2A&AssociateTag=lookandcodego-21&Keywords=Bilder&Operation=ItemSearch&ResponseGroup=Images%2CItemAttributes%2COffers&SearchIndex=All&Service=AWSECommerceService&Timestamp=2018-07-15T13%3A39%3A54.000Z&Signature=o9U6vwJqCm5OEVmcdCRWtBTrqKzgF31sbzxhnny3tb8%3D"
 var request_text3 = "https://webservices.amazon.com/onca/xml?Service=AWSECommerceService&Operation=ItemLookup&ResponseGroup=Images&IdType=ASIN&&ItemId=B004HO6I4M&AWSAccessKeyId=AKIAJMVPMMG6P7HCWLPQ&AssociateTag=codenundgutau-21&Timestamp=[YYYY-MM-DDThh:mm:ssZ]&Signature=[Request_Signature]"
 //xhttp.open("GET", "http://www.omdbapi.com/?i=tt3896198&apikey=28a5839e", true);
 xhttp.open("GET", request_text2, true);
